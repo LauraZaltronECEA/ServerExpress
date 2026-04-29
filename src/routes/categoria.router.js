f4c5cfc656551e8c4b6fae564c8c65c97816f22b
unknown <lazaltron@fecea.edu.ar>
2026-04-28 21:04:38 -0300

    fix: Rutas de categoria

--- src/routes/categoria.router.js
+++ src/routes/categoria.router.js
@@ -6,9 +6,4 @@ const categoriaRouter = express.Router()
 categoriaRouter.post('/', postCategoria)
 categoriaRouter.get('/', getCategoria)
 
-categoriaRouter.get('/', (req, res) =>{
-    const categorias = Object.keys(productos.infoProductos)
-    res.end(JSON.stringify(categorias))
-}) //agregado nuevo en branch feat/get-categorias
-
 module.exports = categoriaRouter
