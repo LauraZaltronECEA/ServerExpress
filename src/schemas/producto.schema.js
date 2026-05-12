c74f4155d078713003c9565a5f432ff0499aad36
unknown <lazaltron@fecea.edu.ar>
2026-05-12 18:52:54 -0300

    fix: nombres de message de joi en schema

--- src/schemas/producto.schema.js
+++ src/schemas/producto.schema.js
@@ -3,18 +3,18 @@ const joi = require('joi')
 //atributos del objeto sean objetos de joi
 const id = joi.number().min(1)
 
-const descripcion = joi.string().min(3).max(15).strip().message({
+const descripcion = joi.string().min(3).max(15).required().messages({
     'string.min': 'La descripcion debe tener por lo menos {#limit} caracteres',
     'string.max': 'La descripcion debe tener como maximo {#limit} caracteres',
     'any.required': 'La descripcion es obligatoria'
-})//mensajes personalizados para q el output no este en ingles
+}).strip();//mensajes personalizados para q el output no este en ingles
 
-const precio = joi.number().min(100).message({
+const precio = joi.number().min(100).messages({
     'number.min': 'El precio debe ser igual o mayor a {#limit}',
     'any.required': 'El precio es obligatorio'
 })//mensajes personalizados para q el output no este en ingles
 
-const categoria = joi.string().min(3).max(15).required().message({
+const categoria = joi.string().min(3).max(15).required().messages({
     'string.min': 'La categoria debe tener por lo menos {#limit} caracteres',
     'string.max': 'La categoria debe tener como maximo {#limit} caracteres',
     'any.required': 'La categoria es obligatoria'
